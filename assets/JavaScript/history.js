4642463a0281bd61fb33ebd643e8e965b5175745
rinat <atkbrc354@mail.ru>
2023-08-21 11:51:28 +0300

    calendar html css

--- assets/JavaScript/history.js
+++ assets/JavaScript/history.js
@@ -1,58 +1,75 @@
-function createCalendar() {
-    var calendar = document.querySelector("#calendar-body");
-    var date = new Date();
-    var month = date.getMonth();
-    var year = date.getFullYear();
-  
-    var monthNames = [
-      "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
-      "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"
-    ];
-  
-    var daysInMonth = new Date(year, month + 1, 0).getDate();
-    var firstDayOfMonth = new Date(year, month, 1).getDay();
-  
-    document.querySelector(".month-year").textContent = monthNames[month] + " " + year;
-  
-    calendar.innerHTML = "";
-  
-    var dateCounter = 1;
-    for (var row = 0; row < 6; row++) {
-      var newRow = document.createElement("tr");
-  
-      for (var col = 0; col < 7; col++) {
-        var newCell = document.createElement("td");
-  
-        if ((row === 0 && col < firstDayOfMonth) || dateCounter > daysInMonth) {
-          newCell.textContent = "";
-        } else {
-          newCell.textContent = dateCounter;
-          newCell.addEventListener("click", function() {
-            alert("Вы выбрали " + monthNames[month] + " " + this.textContent + ", " + year);
-          });
-          dateCounter++;
-        }
-  
-        newRow.appendChild(newCell);
+const calendarBtn = document.querySelector('.calendar-btn');
+const calendars = document.querySelector('.calendars');
+const calendarButtonLeft = document.querySelector('.calendar-button-left');
+
+
+calendarBtn.addEventListener('click', () => {
+  calendars.classList.add('open');
+});
+
+calendarButtonLeft.addEventListener('click', () => {
+  calendars.classList.remove('open');
+});
+
+
+function createCalendar(calendarId) {
+  var calendar = document.querySelector("#" + calendarId);
+  var date = new Date();
+  var month = date.getMonth();
+  var year = date.getFullYear();
+
+  var monthNames = [
+    "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
+    "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"
+  ];
+
+  var daysInMonth = new Date(year, month + 1, 0).getDate();
+  var firstDayOfMonth = new Date(year, month, 1).getDay();
+
+  document.querySelector(".month-year").textContent = monthNames[month] + " " + year;
+  document.querySelector(".month-year-2").textContent = monthNames[month] + " " + year;
+
+  calendar.innerHTML = "";
+
+  var dateCounter = 1;
+  for (var row = 0; row < 6; row++) {
+    var newRow = document.createElement("tr");
+
+    for (var col = 0; col < 7; col++) {
+      var newCell = document.createElement("td");
+
+      if ((row === 0 && col < firstDayOfMonth) || dateCounter > daysInMonth) {
+        newCell.textContent = "";
+      } else {
+        newCell.textContent = dateCounter;
+        newCell.addEventListener("click", function() {
+        });
+        dateCounter++;
       }
-  
-      calendar.appendChild(newRow);
+
+      newRow.appendChild(newCell);
     }
+
+    calendar.appendChild(newRow);
   }
-  
-  function prevMonth() {
-    var date = new Date();
-    date.setMonth(date.getMonth() - 1);
-    createCalendar(date.getMonth(), date.getFullYear());
-  }
-  
-  function nextMonth() {
-    var date = new Date();
-    date.setMonth(date.getMonth() + 1);
-    createCalendar(date.getMonth(), date.getFullYear());
-  }
-  
-  // Создание календаря при загрузке страницы
-  window.onload = function() {
-    createCalendar();
-  };
+}
+
+function prevMonth() {
+  var date = new Date();
+  date.setMonth(date.getMonth() - 1);
+  createCalendar("calendar-body");
+  createCalendar("calendar2");
+}
+
+function nextMonth() {
+  var date = new Date();
+  date.setMonth(date.getMonth() + 1);
+  createCalendar("calendar-body");
+  createCalendar("calendar2");
+}
+
+// Создание календаря при загрузке страницы
+window.onload = function() {
+  createCalendar("calendar-body");
+  createCalendar("calendar2");
+};
